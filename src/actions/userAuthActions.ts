3f7c2cccfecf0460e859f4a848c321f64039694f
scott-t-robinson <55903056+scott-t-robinson@users.noreply.github.com>
2023-02-22 15:43:39 -0800

    Add support for Firebase, basic auth actions, rough draft for core data types (#1)
    * Add support for Firebase, basic auth actions, rough draft for core data types
    
    * Address comments - cleanup unused firbase infra, move interface files, etc
    
    * Swap string declarations to primitives
    
    ---------
    
    Co-authored-by: Scott Robinson <robinnsc@amazon.com>

---
+++ src/actions/userAuthActions.ts
@@ -0,0 +1,63 @@
+import { firebaseApp, firestore } from '../firebase'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import {TEST_ORGANIZATION} from "../interfaces/organization";
+import {TEST_GROUP} from "../interfaces/group";
+import {UserCredential} from "@firebase/auth";
+import {UserEmailCredentials, USERS_COLLECTION} from "../interfaces/user";
+
+const auth = getAuth(firebaseApp);
+
+export const signUp = (userEmailCredentials: UserEmailCredentials) => {
+    const {email, password} = userEmailCredentials;
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((firebaseUserCredential: UserCredential) => {
+            const userId: string = firebaseUserCredential.user.uid;
+            setDoc(doc(firestore, USERS_COLLECTION, userId), {
+               organizationId: [TEST_ORGANIZATION],
+               groupsIds: [TEST_GROUP]
+            }).then(() => {
+
+            });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+}
+
+export const signIn = (userEmailCredentials: UserEmailCredentials) => {
+    const {email, password} = userEmailCredentials;
+    signInWithEmailAndPassword(auth, email, password)
+        .then((firebaseUserCredential: UserCredential) => {
+            // Signed in
+            const user = firebaseUserCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+export const signInWithGoogle = () => {
+    const googleAuthProvider: GoogleAuthProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleAuthProvider)
+        .then((firebaseUserCredential: UserCredential) => {
+            const user = firebaseUserCredential.user;
+            // ...
+        }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+    });
+}
+
+export const signOut = () => {
+    auth.signOut().then(()=>{
+        console.log("Signed out")
+    });
+}
---
+++ src/interfaces/user.ts
@@ -0,0 +1,12 @@
+export const USERS_COLLECTION = "users"
+export interface User {
+    organizationId: string;
+    groupsIds: string[]
+
+    // Other misc user metadata
+}
+
+export interface UserEmailCredentials {
+    email: string;
+    password: string;
+}
