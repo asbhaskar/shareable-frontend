181cd29662d5a671affdbfe1395057eaa29ffc12
Scott Robinson <robinnsc@amazon.com>
2023-03-05 19:40:43 -0800

    Add support for more FiledRequest and Insight Firestore actions

--- src/interfaces/insight.ts
+++ src/interfaces/insight.ts
@@ -4,11 +4,11 @@ import {User} from "./user";
 export const INSIGHTS_COLLECTION = "insights"
 
 export interface Insight {
-    createdBy: User;
+    createdBy: string; // userId
     createDate: number;
     title: string;
-    request: FiledRequest;
-    collaborators: User[];
+    request: string[]; // filedRequestIds
+    collaborators: string[] // userIds/groupsIds;
     description?: string;
     attachments?: string[]; // TODO Support complex media types
     repositoryUrl?: string;
