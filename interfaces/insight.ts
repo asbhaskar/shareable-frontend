275bce9b41560bdd8a97e978c1c7aa68ae1883ae
Abhinav Bhaskar <abhinav@asbhaskar.com>
2023-03-12 00:02:55 -0600

    fix: adding interface files to commit

---
+++ interfaces/insight.ts
@@ -0,0 +1,16 @@
+import {FiledRequest} from "./filedRequest";
+import {User} from "./user";
+
+export const INSIGHTS_COLLECTION = "insights"
+
+export interface Insight {
+    createdBy: User;
+    createDate: number;
+    title: string;
+    request: FiledRequest;
+    collaborators: User[];
+    description?: string;
+    attachments?: string[]; // TODO Support complex media types
+    repositoryUrl?: string;
+    comments?: string[];
+}
---
+++ interfaces/organization.ts
@@ -0,0 +1,10 @@
+import {Group} from "./group";
+
+export const ORGANIZATIONS_COLLECTION = "organizations"
+export const TEST_ORGANIZATION = "TEST_ORGANIZATION"
+
+export interface Organization {
+    Groups: Group[]
+
+    // Other misc org data
+}
