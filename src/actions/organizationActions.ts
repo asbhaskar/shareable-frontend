45aac405fe987057e2c941e1defccd082cfe50a4
scott-t-robinson <55903056+scott-t-robinson@users.noreply.github.com>
2023-05-14 16:18:44 -0700

    Add support for Group, Organization actions. Refactor relational fields within Group, Organization (#6)
    Co-authored-by: Scott Robinson <robinnsc@amazon.com>

---
+++ src/actions/organizationActions.ts
@@ -0,0 +1,39 @@
+import { firestore } from '../firebase'
+import {v4 as uuidv4} from "uuid";
+import {doc, updateDoc, arrayUnion, arrayRemove, getDoc, setDoc} from "firebase/firestore";
+import {Organization, ORGANIZATIONS_COLLECTION} from "../../interfaces/organization";
+
+export const addOrganization = (organization: Organization) => {
+    const uuid: string = uuidv4()
+
+    setDoc(doc(firestore, ORGANIZATIONS_COLLECTION, uuid), organization)
+        .catch((error) => {
+            console.log("Error - FireStore - Adding Organization: ", error);
+        });
+}
+export const getOrganization = (organizationId: string): Organization | null => {
+    getDoc(doc(firestore, ORGANIZATIONS_COLLECTION, organizationId))
+        .then((documentSnapshot) => {
+            return documentSnapshot.data() as Organization
+        })
+        .catch((error) => {
+            console.log("Error - FireStore - Getting Organization: ", error);
+        });
+    return null;
+}
+
+export const addExecutiveToOrganization = (organizationId: string, userId: string) => {
+    updateDoc(doc(firestore, `${ORGANIZATIONS_COLLECTION}/${organizationId}`), {
+        executives: arrayUnion(userId)
+    }).catch((error) => {
+        console.log("Error - FireStore - Adding executive to organization: ", error);
+    });
+}
+
+export const removeExecutiveFromOrganization = (organizationId: string, userId: string) => {
+    updateDoc(doc(firestore, `${ORGANIZATIONS_COLLECTION}/${organizationId}`), {
+        executives: arrayRemove(userId)
+    }).catch((error) => {
+        console.log("Error - FireStore - Removing executive from organization: ", error);
+    });
+}
