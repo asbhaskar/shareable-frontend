280db19271672b356469047cfc79eb9479d1fde1
Scott Robinson <robinnsc@amazon.com>
2023-05-22 17:03:49 -0700

    Integrate basic FiledRequests/Insights functionality into UI Fetch and display FiledRequests/Insights Support UI creation/deletion FiledRequests/Insights Add swappable view between FiledRequests/Insights Refactor FireStore functions to async/await Reconcile 2 existing Insight interface variants into 1

--- src/components/TicketFeedContainer/TicketFeedContainer.tsx
+++ src/components/TicketFeedContainer/TicketFeedContainer.tsx
@@ -1,26 +1,49 @@
-import { Box } from "@mui/material"
-import { InsightDataInterface } from "../../../interfaces/InsightData"
+import {Box, Button} from "@mui/material"
 import InsightCard from "../InsightCard/InsightCard"
+import {Insight} from "@interfaces/insight";
+import {FiledRequest} from "@interfaces/filedRequest";
+import {useState} from "react";
+import FiledRequestCard from "@components/FiledRequestCard/FiledRequestCard";
 
-interface TicketFeedContainerInterface { 
-    insightData: InsightDataInterface[]
+interface TicketFeedContainerInterface {
+    filedRequestData: {[id: string]: FiledRequest},
+    deleteFiledRequestHandlerGenerator: Function,
+    insightData: {[id: string]: Insight},
+    deleteInsightHandlerGenerator: Function
 }
 
-const TicketFeedContainer = ({ insightData }: TicketFeedContainerInterface) => {
+type DisplayType = "Requests" | "Insights"
+
+const TicketFeedContainer = ({   filedRequestData,
+                                 deleteFiledRequestHandlerGenerator,
+                                 insightData,
+                                 deleteInsightHandlerGenerator }: TicketFeedContainerInterface) => {
+    const [currentDisplay, setCurrentDisplay] = useState<DisplayType>("Requests");
+
+    console.log(filedRequestData)
+    console.log(Object.entries(filedRequestData))
     return (
-        <Box sx={{
-            border: '1px #000 solid', 
-            background: '#fff', 
-            width: '100vw', 
-            height:'100vh',
-            marginTop:'100px'
-            }}>
+        <Box>
             <p style={{color: "#000", textAlign: 'end', padding:'1rem 2rem'}}>Sort by Recommended</p>
+            {/**TODO: Current button placement is very cursed, need to adjust**/}
+            <Button  onClick={() => {setCurrentDisplay("Requests");} }>
+                Show Requests
+            </Button>
+            <Button  onClick={() => {setCurrentDisplay("Insights");} }>
+                Show Insights
+            </Button>
+            {currentDisplay == "Requests" &&
             <Box sx={{margin:'2rem auto', display:'flex', flexDirection:'column', alignItems:'center'}}>
-                {insightData.map((insight) => (
-                    <InsightCard insightCardData={insight}/>
+                {Object.entries(filedRequestData).map(([filedRequestId, filedRequest]) => (
+                    <FiledRequestCard filedRequestCardData={filedRequest} onDelete={deleteFiledRequestHandlerGenerator(filedRequestId)}/>
                 ))}
-            </Box>
+            </Box>}
+            {currentDisplay == "Insights" &&
+            <Box sx={{margin:'2rem auto', display:'flex', flexDirection:'column', alignItems:'center'}}>
+                {Object.entries(insightData).map(([insightId, insight]) => (
+                    <InsightCard insightCardData={insight} onDelete={deleteInsightHandlerGenerator(insightId)}/>
+                ))}
+            </Box>}
         </Box>
     )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,35 +1,188 @@
 import TicketFeedContainer from '../components/TicketFeedContainer/TicketFeedContainer'
-import { InsightDataInterface } from '../../interfaces/InsightData'
 import Logo from '../assets/images/shareable-logo.png'
+import {addInsight, deleteInsight, getInsights} from "../actions/insightActions";
+import {TEST_ORGANIZATION} from "@interfaces/organization";
+import {TEST_GROUP} from "@interfaces/group";
+import {Insight} from "@interfaces/insight";
+import {useEffect, useState} from "react";
+import {Box, Button} from "@mui/material";
+import {addFiledRequest, getFiledRequests} from "../actions/filedRequestActions";
+import {FiledRequest} from "@interfaces/filedRequest";
 
-const InsightData: InsightDataInterface[] = [
-    {
+const demoInsights: {[id: string]: Insight} = {
+    "insightId1": {
+        createdBy: 'userId',
         ticketId: 'DATASCI-2864:',
-        title: 'Title blah blah blah',
-        date: '10/2/2011',
+        title: 'DEMO - Title blah blah blah',
+        createDate: 1,
+        lastUpdated: 2,
         imgs: [{src: Logo, alt: 'google image'}],
         outcomeNumber: 21,
         keyStat: '21% increase per annum',
         keyNumber: '21%',
         tldr: 'string string string string string string string string string ',
-        takeaways: 'string string string string string string string string'
+        takeaways: 'string string string string string string string string',
+        requests: ['RequestId1'],
+        collaborators: ['UserId1']
     },
-    {
+    "insightId2": {
+        createdBy: 'userId2',
         ticketId: 'DATASCI-2202:',
-        title: 'Title blah blah blah',
-        date: '8/3/2010',
+        title: 'DEMO - Title blah blah blah',
+        createDate: 3,
+        lastUpdated: 4,
         imgs: [{src: Logo, alt: 'google image'}],
-        outcomeNumber: 1,
+        outcomeNumber: 25,
         keyStat: '25% increase per annum',
         keyNumber: '25%',
         tldr: 'string string string string string string string string string ',
-        takeaways: 'string string string string string string string string'
+        takeaways: 'string string string string string string string string',
+        requests: ['RequestId2'],
+        collaborators: ['UserId2']
+    }
+}
+
+const demoRequests: {[id: string]: FiledRequest} = {
+    "requestId1": {
+        createdBy: "BigBossId",
+        createDate: 123,
+        lastUpdated: 123,
+        title: "DEMO - Title blah blah blah",
+        assignee: "CodeMonkeyId",
+        priority: "High",
+        status: "Assigned",
+        deadline: 123
     },
-]
+    "requestId2": {
+        createdBy: "BigBossId",
+        createDate: 123,
+        lastUpdated: 123,
+        title: "DEMO - Title blah blah blah",
+        assignee: "CodeMonkeyId",
+        priority: "High",
+        status: "Assigned",
+        deadline: 123
+    }
+}
 
 const Dashboard = () => {
+    const [displayedRequests, setDisplayedRequests] = useState<{[id: string]: FiledRequest}>(demoRequests);
+    const [displayedInsights, setDisplayedInsights] = useState<{[id: string]: Insight}>(demoInsights);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const storedRequests = await getFiledRequests(TEST_ORGANIZATION, TEST_GROUP);
+                // TODO: Refactor, current state is a bit hacky to force React to update
+                setDisplayedRequests({...displayedRequests, ...storedRequests});
+            } catch (error) {
+                console.log("Fetching requests broken very sad")
+            }
+            try {
+                const storedInsights = await getInsights(TEST_ORGANIZATION, TEST_GROUP);
+                // TODO: Refactor, current state is a bit hacky to force React to update
+                setDisplayedInsights({...displayedInsights, ...storedInsights});
+            } catch (error) {
+                console.log("Fetching insights broken very sad")
+            }
+        })();
+    }, [])
+
+    // Generate and store hardcoded insight object for now
+    const createNewInsight = async () => {
+        const hardcodedInsight: Insight = {
+            createdBy: 'userId',
+            createDate: 1,
+            lastUpdated: 2,
+            ticketId: 'DATASCI-2864:',
+            title: 'FireBase - Title blah blah blah',
+            imgs: [{src: Logo, alt: 'google image'}],
+            outcomeNumber: 21,
+            keyStat: '21% increase per annum',
+            keyNumber: '21%',
+            tldr: 'string string string string string string string string string',
+            takeaways: 'string string string string string string string string',
+            requests: ['RequestId1'],
+            collaborators: ['UserId1']
+        };
+        try {
+            const insightId: string = await addInsight(TEST_ORGANIZATION, TEST_GROUP, hardcodedInsight);
+            displayedInsights[insightId] = hardcodedInsight;
+            setDisplayedInsights({...displayedInsights});
+        } catch (error) {
+            // Replace with user visible messaging
+            console.log("Could not create new insight");
+        }
+    }
+
+    // Generate and store hardcoded request object for now
+    const createNewFiledRequest = async () => {
+        const hardcodedFiledRequest: FiledRequest = {
+            createdBy: "BigBossId",
+            createDate: 123,
+            lastUpdated: 123,
+            title: "FireBase - Title blah blah blah",
+            assignee: "CodeMonkeyId",
+            priority: "High",
+            status: "Assigned",
+            deadline: 123
+        };
+        try {
+            const filedRequestId: string = await addFiledRequest(TEST_ORGANIZATION, TEST_GROUP, hardcodedFiledRequest);
+            displayedRequests[filedRequestId] = hardcodedFiledRequest;
+            // TODO: Refactor, current state is a bit hacky to force React to update
+            setDisplayedRequests({...displayedRequests});
+        } catch (error) {
+            // Replace with user visible messaging
+            console.log("Could not create new request");
+        }
+    }
+
+    const generateDeleteFiledRequestHandler = (filedRequestId: string): Function => {
+        return async () => {
+            try {
+                await deleteInsight(TEST_ORGANIZATION, TEST_GROUP, filedRequestId);
+                delete displayedRequests[filedRequestId];
+                // TODO: Refactor, current state is a bit hacky to force React to update
+                setDisplayedRequests({...displayedRequests});
+            } catch {
+                console.log("Could not delete filed request");
+            }
+        }
+    };
+
+    const generateDeleteInsightHandler = (insightId: string): Function => {
+        return async () => {
+            try {
+                await deleteInsight(TEST_ORGANIZATION, TEST_GROUP, insightId);
+                delete displayedInsights[insightId];
+                setDisplayedInsights({...displayedInsights});
+            } catch {
+                console.log("Could not delete insight");
+            }
+        }
+    };
+
     return (
-        <TicketFeedContainer insightData={InsightData}/>
+        <Box sx={{
+            border: '1px #000 solid',
+            background: '#fff',
+            width: '100vw',
+            height:'100vh',
+            marginTop:'100px'
+        }}>
+            {/**TODO: Current button placement is very cursed, need to adjust**/}
+            <Button onClick={async () => {await createNewFiledRequest();}}>
+                New Request
+            </Button>
+            <Button onClick={async () => {await createNewInsight();}}>
+                New Insight
+            </Button>
+            <TicketFeedContainer filedRequestData={displayedRequests}
+                                 deleteFiledRequestHandlerGenerator={generateDeleteFiledRequestHandler}
+                                 insightData={displayedInsights}
+                                 deleteInsightHandlerGenerator={generateDeleteInsightHandler}/>
+        </Box>
     )
 }
 
