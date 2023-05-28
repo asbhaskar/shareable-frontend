1ca8f42b6542729590eafe3a55dd9581449a3a99
Abhinav Bhaskar <abhinav@asbhaskar.com>
2023-05-28 18:50:12 -0500

    feat: adding ts linting for unused vars

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+
+module.exports = exports = {
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+      "@typescript-eslint",
+    ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    "rules": {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    },
+    "root": true
+}
